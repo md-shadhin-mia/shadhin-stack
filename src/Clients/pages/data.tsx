cef5dd5122355b208532d36e54728212bf739a1c
md_shadhin <mdshadhinkn@gmail.com>
2023-10-19 07:36:47 +0600

    add new data

---
+++ src/Clients/pages/data.tsx
@@ -0,0 +1,76 @@
+const data ={
+    name:"Md Shadhin Mia",
+    email:"mshadhinkn@gmail.com",
+    title:"Software Engineer",
+    phone:"0130-2591377",
+    address:"Dhaka, Bangladesh",
+    coverImage: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    objective:"To obtain a software engineer position that allows me to utilize my strong coding skills Java and Javascript to develop high-quality web applications that meet the client’s needs and expectations.",
+    skills:[
+        "Java",
+        "Javascript",
+        "Spring boot",
+        "Spring Data JPA",
+        "Hibernate",
+        "Mysql",
+        "MongoDB",
+        "PostgreSQL",
+        "HTML",
+        "CSS",
+        "React",
+        "Node.js",
+        "Bootstrap",
+        "Tailwind CSS",
+        "Git",
+        "Docker",
+    ],
+    experience:[
+        {
+            title:"Software Engineer",
+            company:"Smart Software Limited",
+            from:"May 1 2023",
+            to:"Continue",
+            responsibilities:"As a Software Engineer at Smart Software, I specialized in ERP (Enterprise Resource Planning) software development. My role involved designing, customizing, and integrating ERP solutions, collaborating with cross-functional teams to meet client requirements. I excelled in database design, optimizing system performance, and ensuring a seamless user interface. I conducted rigorous quality assurance, documented technical details, and worked closely with clients to provide ongoing support."
+        },
+        {
+            title: "Software Engineer",
+            company: "SuperbNexus Limited",
+            from: "Dec 1 2022",
+            to: "May 1 2023",
+            responsibilities: "I worked on a team that developed a web app using Spring Boot and React. I collaborated on both the front and back-end, developed the RESTful API, Microservices, and designed React components. I participated in ERP System and E-commerce project writing code, reviews, bug fixing project progress.",
+        },
+        {
+            title: "Java Developer Intern",
+            company: "Daffodil International Academy",
+            from: "Sep 1 2022",
+            to: "Dec 1 2022",
+            responsibilities: "Assisted with development Java Application and solving problem with structured way. Analyzing problems and writing programs from its. Collaborated with the development team"
+        }
+    ],
+    education:[
+        {
+            institution: "Dhaka Polytechnic Institute",
+            degree: "Diploma in Computer Engineering",
+            fieldOfStudy: "Computer Science",
+            from: "Aug 1 2018",
+            to: "Dec 1 2022",
+            gpa: "3.69 out of 4.0",
+        },
+        {
+            institution: "Gazipur Technical Institute",
+            degree: "SSC(VOC)",
+            fieldOfStudy: "Com",
+            from: "Jan 1 2016",
+            to: "Jan 1 2018",
+            gpa: "3.69 out of 4.0",
+        }
+    ],
+    languages:[
+        {
+
+        }
+    ],
+    activity:""
+}
+
+export default data;
